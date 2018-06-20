df0323da58aca4679c83f9a89226bf30b3274e50
thoumc <thoumc@hotmail.com>
2018-06-20 01:39:27 +0000

    hovering feature added

--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $(".tweet-block").hover(
+  $(".tweet-article").hover(
     function(){
-      $(this).children("tweet-icons").css('opacity', 1.0);
+      $(this).find(".tweet-icons").css('opacity', 1.0);
     }, function(){
-      $(this).children("tweet-icons").css('opacity', 0.0);
+      $(this).find(".tweet-icons").css('opacity', 0.0);
     })
 
 });
