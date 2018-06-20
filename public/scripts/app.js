e7a1e30dc4738ba1eabb59db2324185a1f27ec44
thoumc <thoumc@hotmail.com>
2018-06-20 22:18:16 +0000

    tweet reload

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -43,19 +43,42 @@ function loadTweets(){
   $.ajax({
     url: '/tweets',
     type: 'GET'
-  }).then(function(jsonContent){
+  }).success(function(jsonContent){
     renderTweets(jsonContent);
     });
 }
 
 
-
-
+function ajaxSubmitPost (data){
+  $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: data
+      }).done(function(){
+        loadTweets();
+      });
+}
 
 $(document).ready(function(){
 
- loadTweets()
+ loadTweets();
 
-})
+ $( "form" ).on( "submit", function( event ) {
+
+  event.preventDefault();
+
+  var $textNumber = $(this).find("textarea").val().length;
+  if ( $textNumber === 0){
+    alert("You need to enter something to submit!");
+  } else if ($textNumber > 140) {
+    alert("The maximum character is 140!!")
+  } else {
+    let tweetData = $(this).find("textarea").serialize();
+    ajaxSubmitPost(tweetData);
+    $(this).find("textarea").val('');
+  }
+});
+
+});
 
 
--- public/scripts/form-action.js
+++ public/scripts/form-action.js
@@ -1,26 +1,6 @@
 $(document).ready(function(){
-  $( "form" ).on( "submit", function( event ) {
-    event.preventDefault();
-    var $textNumber = $(this).find("textarea").val().length;
-    if ( $textNumber === 0){
-      alert("You need to enter something to submit!");
-    } else if ($textNumber > 140) {
-      alert("The maximum character is 140!!")
-    } else {
-      let tweetData = $(this).find("textarea").serialize();
-      ajaxSubmitPost(tweetData);
-    }
-  })
+// refactor this part from app.js later
 });
 
-
-
-function ajaxSubmitPost (data){
-  $.ajax({
-        method: 'POST',
-        url: '/tweets',
-        data: data
-      });
-}
 
 
